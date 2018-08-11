03c626054240086e144c8fea44f4abab829731fb
Jordan Martin <jdordan23@gmail.com>
2018-08-11 20:47:53 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -23,6 +23,7 @@ function doWhileLoop(array) {
   }
   do {
     array.pop()
-  } while (array.length > 0 && incrementVarialbe())
+    incrementVarialbe()
+  } while (array.length > 0)
   return array
 }
